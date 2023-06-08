c74e062960741174c25503b1ac0d889656d8abc9
Vlad Mikhnenok <vlad.mikhnenok.95@gmail.com>
2023-06-08 16:56:04 +0300

    Добавлен новый шрифт, добавлен bottom_navigation, добавлен toolbar

--- app/src/main/java/ml/vladmikh/projects/food_shop/MainActivity.kt
+++ app/src/main/java/ml/vladmikh/projects/food_shop/MainActivity.kt
@@ -2,10 +2,35 @@ package ml.vladmikh.projects.food_shop
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.NavController
+import androidx.navigation.Navigation
+import androidx.navigation.findNavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.NavigationUI.setupWithNavController
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
+import ml.vladmikh.projects.food_shop.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityMainBinding
+    //private lateinit var navController: NavController
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding= ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
+
+        val navHostFragment =
+            supportFragmentManager.findFragmentById(R.id.navHostFragment) as NavHostFragment
+        val navController: NavController = navHostFragment.navController
+        //setupActionBarWithNavController(navController)
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.startFragment, R.id.basketFragment))
+        binding.toolBar.setupWithNavController(navController, appBarConfiguration)
+
+        setupWithNavController(binding.bottomNavigation,navController)
+
     }
 }
--- app/src/main/java/ml/vladmikh/projects/food_shop/ui/start/StartFragment.kt
+++ app/src/main/java/ml/vladmikh/projects/food_shop/ui/start/StartFragment.kt
@@ -5,17 +5,39 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.Navigation
+import androidx.navigation.fragment.findNavController
 import ml.vladmikh.projects.food_shop.R
+import ml.vladmikh.projects.food_shop.databinding.FragmentStartBinding
 
 class StartFragment : Fragment() {
 
+    private lateinit var binding: FragmentStartBinding
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
-    ): View? {
+    ): View {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_start, container, false)
+
+        binding = FragmentStartBinding.inflate(inflater, container, false)
+
+        binding.bakeries.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_categoryFragment, null)
+        )
+
+        binding.fastFood.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_categoryFragment, null)
+        )
+
+        binding.asian.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_categoryFragment, null)
+        )
+
+        binding.soup.setOnClickListener (
+            Navigation.createNavigateOnClickListener(R.id.action_startFragment_to_categoryFragment,null)
+        )
+        return binding.root
     }
 
 }
